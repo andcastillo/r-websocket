d277805722ad5171876bf962f89f119698662e81
andcastillo <andcastillo@gmail.com>
2019-08-13 21:50:12 -0500

    add visualizer

--- rest_controller.R
+++ rest_controller.R
@@ -1,6 +1,9 @@
 # The enviroment for these routes
 e <- new.env()
 
+#* @assets ./visualizer /goo
+list()
+
 #* @filter cors
 cors <- function(req, res) {
   res$setHeader("Access-Control-Allow-Origin", "*")
